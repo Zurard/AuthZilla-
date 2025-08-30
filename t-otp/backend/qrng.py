92a0b09e39841f7c7384821b063dfa3aec45c825
Zurard <zurard07@gmail.com>
2025-08-30 19:08:46 +0530

    qrng basic model ready

--- t-otp/backend/qrng.py
+++ t-otp/backend/qrng.py
@@ -5,6 +5,18 @@ MAX_SIZE = 256 #for a seed of the max size
 from qiskit import QuantumCircuit,transpile
 from qiskit_aer import AerSimulator
 
+def qrng_random_int(bytes):
+    rng =  []
+    n=200
+    max_val = 10
+    while ( n >= 0):
+        bits = generate_random_bits(16)
+        num = int(''.join(map(str, bits)), 2)
+        # print(num % max_val)
+        rng.append(num % max_val)
+        n -= 1
+    return rng
+
 def generate_random_bits(n_bits):
     bits = []
     sim = AerSimulator()
@@ -22,6 +34,18 @@ def generate_random_bits(n_bits):
         n_bits -= 1
     return bits
 
+def bits_to_bytes(bits):
+    bytes_list = []
+    for i in range(0, len(bits), 8):
+        byte = 0
+        for j in range(8):
+            if i + j < len(bits):
+                byte = (byte << 1) | bits[i + j]
+        bytes_list.append(byte)
+    return bytes_list
+
 
 bits = generate_random_bits(100)
-print(bits)
+bytes = bits_to_bytes(bits)
+rng = qrng_random_int(bytes)
+print(rng)
